36c2b403539bf7db7afe62a2da0f9c01611e37c2
JiangXavier <JiangXavier@163.com>
2024-02-24 00:47:11 +0800

    finish lru

--- lru/lru.go
+++ lru/lru.go
@@ -16,10 +16,14 @@ type entry struct {
 }
 
 type Value interface {
-	Len()
+	Len() int
 }
 
-func (c *Cache) New(m int, evicted func(key string, value Value)) *Cache {
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
+
+func New(m int64, evicted func(key string, value Value)) *Cache {
 	return &Cache{
 		MaxBytes:  m,
 		ll:        list.New(),
@@ -28,8 +32,37 @@ func (c *Cache) New(m int, evicted func(key string, value Value)) *Cache {
 	}
 }
 
-func (c *Cache) Add(key string, value Value) {}
+func (c *Cache) Add(key string, value Value) {
+	if node, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(node)
+		c.usedByte += int64(value.Len()) - int64(node.Value.(*entry).value.Len())
+		node.Value.(*entry).value = value
+	} else {
+		node = c.ll.PushFront(&entry{key, value})
+		c.cache[key] = node
+		c.usedByte += int64(len(key)) + int64(value.Len())
+	}
+	for c.MaxBytes != 0 && c.usedByte > c.MaxBytes {
+		c.RemoveOld()
+	}
+}
 
-func (c *Cache) RemoveOld() {}
+func (c *Cache) RemoveOld() {
+	node := c.ll.Back()
+	if node != nil {
+		c.ll.Remove(node)
+		delete(c.cache, node.Value.(*entry).key)
+		c.usedByte -= int64(len(node.Value.(*entry).key)) + int64(node.Value.(*entry).value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(node.Value.(*entry).key, node.Value.(*entry).value)
+		}
+	}
+}
 
-func (c *Cache) Get(key string) {}
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if node, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(node)
+		return node.Value.(*entry).value, ok
+	}
+	return
+}
