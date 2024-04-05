ad3f7bc12076f29874a18fa7d86665530a8e8146
JiangXavier <JiangXavier@163.com>
2024-04-06 00:07:08 +0800

    remake

--- LeiCache/peers.go
+++
@@ -1,9 +0,0 @@
-package LeiCache
-
-type PeerPicker interface {
-	PickPeer(key string) (peer PeerGetter, ok bool)
-}
-
-type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
-}
--- go.mod
+++
@@ -1,7 +0,0 @@
-module example
-
-go 1.21.5
-
-replace Leicache => ./Leicache
-
-require Leicache v0.0.0-00010101000000-000000000000
